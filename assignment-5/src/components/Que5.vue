898cc9c9a4d5f094a49e31974da7de73fa0a97bb
Anu-500apps <anu.taddi@500apps.com>
2022-04-20 17:34:55 +0530

    20-04-22

--- assignment-5/src/components/Que5.vue
+++ assignment-5/src/components/Que5.vue
@@ -2,34 +2,24 @@
   <div>
     <h1 align="center">This is Question-5</h1>
     <h5>Creating APIs</h5>
-
     <div>
       <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
     </div>
-
     <b-button @click="getData()" class="btn btn-success">Click Me!</b-button>
-
     <div>
       <center>
         <table border="2px">
           <tr>
             <th>name</th>
-
             <th>domains</th>
-
             <th>country</th>
-
             <th>web_pages</th>
           </tr>
-
           <tbody>
             <tr v-for="data in searchData.data" :key="data.id">
               <td>{{ data.name }}</td>
-
               <td>{{ data.domains }}</td>
-
               <td>{{ data.country }}</td>
-
               <td>{{ data.web_pages }}</td>
             </tr>
           </tbody>
@@ -42,28 +32,20 @@
 var axios = require("axios").default;
 export default {
   name: "sampleApis",
-
   data() {
     return {
       options: {
         //method: "POST",
-
         url: "http://universities.hipolabs.com/search?country="+this.text,
-
-
         Fetchdata: { search: "" },
       },
-
       text: "",
-
       searchData: "",
     };
   },
-
   methods: {
     async getData() {
       this.options.data.search = this.text;
-
       this.searchData = await axios.request(this.options);
     },
   },
