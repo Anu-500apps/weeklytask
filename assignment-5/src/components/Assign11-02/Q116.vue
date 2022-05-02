bc461472b29facf88153dd59660317680284cb83
Anu-500apps <anu.taddi@500apps.com>
2022-05-02 18:27:08 +0530

    02-05-22

---
+++ assignment-5/src/components/Assign11-02/Q116.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>This is Question-6</h1>
+    <button type="submit" @click="Most">Click</button>
+    <p>{{a1}}{{a2}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "QuE116",
+  data() {
+    return {
+      arr1: [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3],
+      a1: " ",
+      a2: " ",
+    };
+  },
+  methods: {
+    Most() {
+      const res = this.arr1.reduce((x, y) => {
+        if (y in x) {
+          x[y]++;
+        } else {
+          x[y] = 1;
+        }
+        if (x[y] > this.a1) {
+          this.a1 = x[y];
+          this.a2 = y;
+        }
+        return x;
+      }, {});
+    this.a2, this.a1;
+      console.write(res,+"times");
+    },
+  },
+};
+</script>
